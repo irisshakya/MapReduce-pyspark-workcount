150d010a9f60403a0961f843ec438a2c78300561
irisshakya <46196492+irisshakya@users.noreply.github.com>
2022-11-14 15:23:29 -0500

    implemented tf-idf
    remaining is cosine similarity
    added references

--- big_data_proj_2.py
+++ big_data_proj_2.py
@@ -1,8 +1,17 @@
+"""
+    project 2, TF-IDF; cosine similarity 
+    author: Iris Shakya
+    course: Big Data Tech
+    
+    references: 
+            https://dzone.com/articles/calculating-tf-idf-with-apache-spark
+            https://datascience-enthusiast.com/Python/text_analysis.html
+            https://notebook.community/MingChen0919/learning-apache-spark/notebooks/04-miscellaneous/TF-IDF
+"""
 
 
 from pyspark import SparkConf, SparkContext
-from pyspark.mllib.feature import HashingTF
-from pyspark.mllib.feature import IDF
+from pyspark.mllib.feature import HashingTF, IDF
 import string
 import numpy as np
 import pandas as pd
@@ -12,66 +21,78 @@ import gc
 
 
 
-sc = SparkContext(master = 'local')
-spark = SparkSession.builder.appName('pySpark word-count').config('spark.some.config.option', 'some-value').getOrCreate()
+from pyspark.sql import functions as f
+from pyspark.sql.window import Window
+from pyspark.sql.functions import row_number
 
+
+sc = SparkContext(master = 'local')
+spark = SparkSession.builder.appName('pySpark word-count')             .config('spark.some.config.option', 'some-value')             .getOrCreate()
 
 
 data = spark.sparkContext.textFile("/Users/iris/data.txt")
 
 
-# #PRE-PROCESSING
+# SEPARATE EACH NEWLINE AS A list
+my_para = data.flatMap(lambda x: x.split('\n'))
+
+
+my_list = my_para.map(lambda x: list(x.split(" ")))
 
 
 
-#first map to split each term
-res = data.flatMap(lambda x: x.replace("\n", '').split(" "))
-
-#second mapper to append integer to word
-res = res.map(lambda x: (x,1))
-
-#reducer to do the count
-count = res.reduceByKey(lambda x,y: x+y)
-
-print(count.take(3))
+from pyspark.sql import Row
+from pyspark.sql.types import StringType
+from pyspark.sql import functions as F
 
 
-# In[5]:
+row = Row("doc") # Or some other column name
+my_df = my_list.flatMap(row).toDF(schema=StringType())
+my_df.show(truncate=True)
 
 
-#convert to dataframe
-df = spark.createDataFrame(count, ['term', 'count'])
-df = df.filter(df["term"] != '')
-df.show()
+df = my_df.withColumn('doc_id', F.monotonically_increasing_id())
+num_doc = df.count()
 
 
-# In[6]:
+
+from pyspark.sql.functions import col, regexp_replace, split
+
+df_trans = df.withColumn("value",
+split(regexp_replace(col("value"), r"(^\[)|(\]$)|(')", ""), ", "))
+
+#df_trans.printSchema()
+
+#new_df = df_trans.withColumn("token", F.explode("value").alias('token')).show(truncate=True)
+new_df = df_trans.select('*', F.explode('value').alias('token'))
+
+new_df.show()
 
 
-from pyspark.sql.functions import col, lit
-from functools import reduce
 
-# we can edit any prefixes we require 
-prefixes = ['dis_','gene_']
+#CAlCULATE TF
+TF = new_df.groupBy("doc_id").agg(F.count("token").alias("doc_len"))             .join(new_df.groupBy("doc_id", "token")             .agg(F.count("value").alias("word_count")), ['doc_id']) 
 
-# reducer to filter with prefix
-# starts_with = reduce(
-#     lambda x, y: x | y,
-#     [col("term").startswith(s) for s in prefixes], 
-#     lit(False))
+#print(new_df.columns)
 
 
-# In[10]:
+
+TF = TF.withColumn("tf", F.col("word_count") / F.col("doc_len"))       .drop("doc_len", "word_count")
 
 
-h_df=df.where(reduce(
-    lambda x, y: x | y,
-    [col("term").startswith(s) for s in prefixes], 
-    lit(False))).show()
+new_df.show()
+TF.show()
 
 
-# In[8]:
+
+# Calculate IDF
+IDF = new_df.groupBy("token").agg(F.countDistinct("doc_id").alias("df"))
+IDF = IDF.select('*', (F.log(num_doc / (IDF['df'] + 1))).alias('idf'))
+
+# Calculate TF-IDF
+TF_IDF = TF.join(IDF, ['token']).withColumn('tf-idf', F.col('tf') * F.col('idf'))
 
 
-h_df.sort(col("count").desc()).show(truncate=False)
+
+TF_IDF.show()
 
